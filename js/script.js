2e698137dc1e948981ce97b9503bd9babd754b1e
patszy <patszy97@interia.pl>
2021-07-29 14:18:03 +0200

    Js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 fetch('https://patszy.github.io/quiz-game/questions.json')
 .then(response => response.json())
-.then(data => {console.log(data)})
-.catch(error => console.error(error))
+.then(data => {
+    console.log(data);
+    setUp(data);
+})
+.catch(error => console.error(error));
+
+function setUp(data) {
+    let questionAmount = 0;
+    const headerInput = document.querySelector(`.header__input`);
+    const mainQuestion = document.querySelector(`.main__question`);
+
+    headerInput.max = data.length-1;
+
+    headerInput.addEventListener(`input`, (event)=> {
+        questionAmount = event.target.value;
+        if(questionAmount > parseInt(headerInput.max)) {
+            questionAmount = headerInput.max
+            event.target.value = questionAmount;
+        }
+
+        mainQuestion.innerHTML = Object.entries(data[drawNumber(questionAmount)])[0][0];
+    })
+}
+
+function drawNumber(number) {
+    let rand = Math.floor(Math.random()+1*number);
+    return rand;
+}
